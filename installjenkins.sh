ba852d4d84bd1c703f5600ac6eaa7b80cd5301a5
samit-a <adhikarisamit@gmail.com>
2018-08-27 23:57:41 -0500

    Update installjenkins.sh

--- installjenkins.sh
+++ installjenkins.sh
@@ -1,23 +1,23 @@
 #/bin/bash 
 #because i like a clear screen
-  clear
+	clear
 
 #Install the JDK dependency. Jenkins is written in Java so this is a requirement. 
-  sudo yum -y install java-1.8.0-openjdk
+ 	sudo yum -y install java-1.8.0-openjdk
 
 #Get the repository from Jenkins' official URL 
-  sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+ 	sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
 
 #Import the key from RedHat 
-  sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+ 	sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 
 #Installing Jenkins.. 
-  sudo yum -y install jenkins-2.121.1
+ 	sudo yum -y install jenkins-2.121.1
 
 # This command will automatically start Jenkins whenever your system boots 
-  sudo systemctl enable jenkins
+ 	sudo systemctl enable jenkins
 
 #Start the Jenkins service 
-  sudo systemctl start jenkins
+ 	sudo systemctl start jenkins
 
 
