c8ddf49321d86fcd9b9db9daa8e837485bf10fae
sam-it <samit@MyMac.local>
2019-07-28 21:52:50 -0500

    updated

--- installjenkins.sh
+++ installjenkins.sh
@@ -3,20 +3,32 @@
 #because i like a clear screen
 clear
 
+#Run as root user
+if [ "${whoami}" != 'root' ]; then
+ echo "You must be root to run this script!"
+ exit 1
+else
+  echo "Starting.."
+fi
+
 #Install the JDK dependency. Jenkins is written in Java so this is a requirement.
-sudo yum -y install java-1.8.0-openjdk
+yum -y install java-1.8.0-openjdk-devel
 
 #Get the repository from Jenkins' official URL
-sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat-stable/jenkins.repo
+wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat/jenkins.repo
 
 #Import the key from RedHat
-sudo rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
+rpm --import https://jenkins-ci.org/redhat/jenkins-ci.org.key
 
 #Installing Jenkins..
-sudo yum -y install jenkins-2.121.1
+yum install jenkins -y
 
 # This command will automatically start Jenkins whenever your system boots
-sudo systemctl enable jenkins
+systemctl enable jenkins
 
 #Start the Jenkins service
-sudo systemctl start jenkins
+systemctl restart jenkins
+
+echo "Installation complete!"
+
+echo "Here is your initial Jenkins password: `cat /var/lib/jenkins/secrets/initialAdminPassword`"
